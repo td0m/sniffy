6c2c9430a5d572635ec7682662a028c98aa6db44
d0minikt <dominik.t.uk@gmail.com>
2020-02-08 17:16:32 +0000

    add working sniffer & upload data

--- node/wifi.py
+++ node/wifi.py
@@ -1,10 +1,42 @@
 from scapy.all import *
+import threading
+import requests
+import time
+
+endpoint = "http://10.14.138.32:5000/node"
 
 devices = []
 
 def PacketHandler(pkt):
     if pkt.haslayer(Dot11):
-        if pkt.type == 0 and pkt.subtype == 8:
-            print(f"MAC: {pkt.addr2} {str(pkt.info)}")
+        # type 0 checks for management frame
+        # subtype 4 checks for probe request
+        # subtype 8 for beacons
+        if pkt.type == 0 and pkt.subtype == 4:
+            if pkt.addr2 not in devices:
+                signal = pkt.getfieldval('dBm_AntSignal')
+                devices.append({
+                    "state": "new",
+                    "time": time.time(),
+                    "mac": pkt.addr2,
+                    "name": pkt.info.decode("utf-8"),
+                    "strength": signal
+                })
+
+def upload_periodically():
+    while True:
+        global devices
+        data = devices
+        devices = []
+        print("done")
+        try:
+            r = requests.post(url = endpoint, json = data)
+            print(r.text)
+        except Exception as e:
+            print(e)
+        time.sleep(20)
+
+upload_thread = threading.Thread(target=upload_periodically)
+upload_thread.start()
 
 sniff(iface="wlp0s20f0u1", prn = PacketHandler)
