c394869f6e6d85bfde1587f2132a9f92cbf3dcff
lns1st3nt <kunderscore3@gmail.com>
2020-02-09 01:47:08 +0000

    graph updated to be compatible with the framed data

--- client-src/main-frame.js
+++ client-src/main-frame.js
@@ -51,6 +51,7 @@ function names_count_each_frame(frames) {
 function draw_graph(graph_wrapper) {
   let data = graph_wrapper.data;
   let options = graph_wrapper.options;
+  console.log("draw", data, options);
   graph_wrapper.graph.draw(data, options);
 }
 
@@ -78,11 +79,11 @@ function getPieChart()
   };
 
   let chart = new google.visualization.PieChart(document.getElementById('names_piechart'));
-  return {graph: chart, data: undefined, options: options};
+  return {graph: chart, data: undefined, options: options, frame_name_counts: undefined};
 }
 
 function getHeatChartData(total) {
-  console.log(toatl);
+  console.log("total", total);
   return google.visualization.arrayToDataTable([
     ['Traffic', 'Density', { role: 'style' }],
     ['Traffic', total, '#b87333'],            // RGB value
@@ -110,14 +111,21 @@ function getHeatChart()
 }
 
 function getLineChartData(time_series_data) {
-  console.log(time_series_data);
   return google.visualization.arrayToDataTable([
     ['Date', 'Count'],
     ...time_series_data
   ]);
 }
 
-function getLineChart() {
+var device_count_graph = undefined;
+var traffic_graph = undefined;
+var line_graph = undefined;
+
+function updatePieChartToMatchLineChart(e) {
+  e.row;
+}
+
+function getLineChart(device_count_graph) {
   var options = {
     title: 'New devices time line',
     animation:{
@@ -128,39 +136,49 @@ function getLineChart() {
   };
 
   var chart = new google.visualization.LineChart(document.getElementById('line_chart'));
+
+  google.visualization.events.addListener(chart, 'onmouseover', (e) => {
+    frame_name_counts = device_count_graph.frame_name_counts;
+    device_count_graph.data = getPieChartData(frame_name_counts[e.row]);
+    console.log(device_count_graph.data);
+    draw_graph(device_count_graph);
+  });
+
   return {graph: chart, data: undefined, options: options};
 }
-
-var device_count_graph = undefined;
-var traffic_graph = undefined;
-var line_graph = undefined;
 
 function drawChart() {
   if (!device_count_graph)
     device_count_graph = getPieChart();
 
-  if (!traffic_graph)
-    traffic_graph = getHeatChart();
+  // if (!traffic_graph)
+  //   traffic_graph = getHeatChart();
 
   if (!line_graph)
-    line_graph = getLineChart();
+    line_graph = getLineChart(device_count_graph);
 
   (async () => {
     //
     let {data} = await get_all_data();
     let time_series = devices_per_unit_frame(data);
     let counts = names_count_each_frame(data);
+
+    device_count_graph.frame_name_counts = counts;
+
     let total = counts.reduce((acc, x) => x[1] + acc, 0);
 
-    device_count_graph.data = getPieChartData(counts);
-    traffic_graph.data = getHeatChartData(total);
+    if (counts.length > 0) {
+      if (counts[0].length > 0) {
+        console.log(counts[0])
+        device_count_graph.data = getPieChartData(counts[0]);
+        draw_graph(device_count_graph);
+      }
+    }
+
+    // traffic_graph.data = getHeatChartData(total);
+    // draw_graph(traffic_graph);
+
     line_graph.data = getLineChartData(time_series);
-
-    console.log(counts);
-    console.log(time_series);
-
-    draw_graph(device_count_graph);
-    draw_graph(traffic_graph);
     draw_graph(line_graph);
   })();
 }
